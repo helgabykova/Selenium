4005d7a9d520a465dbb704cb698f549a366eaa62
helgabykova <mookkerenok@gmail.com>
2016-08-17 17:39:21 +0300

    Updates2.1

--- ConsoleApplication2/NUnitTest.cs
+++ ConsoleApplication2/NUnitTest.cs
@@ -8,6 +8,8 @@ using System.Collections.ObjectModel;
 using OpenQA.Selenium.Interactions;
 using OpenQA.Selenium.Support.UI;
 using System.Collections.Generic;
+using NUnit.Framework.Interfaces;
+using OpenQA.Selenium.Remote;
 
 namespace ConsoleApplication2
 {
@@ -15,21 +17,24 @@ namespace ConsoleApplication2
     public class NUnitTest
     {
         IWebDriver driver;
-
+        
         [SetUp]
         public void Initialize()
         {
             driver = new ChromeDriver();
+
+            DesiredCapabilities desiredCap = new DesiredCapabilities();
+            desiredCap.SetCapability("platform", "Windows");
         }
         [Test]
         public void PortalLogin()
 
         {
-            driver.Url = "https://portal.globallogic.com";
+            driver.Url = "https://portal.globallogic.com/access/signin";
             IWebElement login = driver.FindElement(By.XPath("//*[@id='app']/div/div/div/div[2]/div/div/form/div[2]/div[1]/input"));
-            login.SendKeys("login");
+            login.SendKeys("olga.bykova");
             IWebElement pass = driver.FindElement(By.XPath("//*[@id='app']/div/div/div/div[2]/div/div/form/div[2]/div[2]/input"));
-            pass.SendKeys("password");
+            pass.SendKeys("Notulp1994");
             IWebElement submit = driver.FindElement(By.XPath("//*[@id='app']/div/div/div/div[2]/div/div/form/button"));
             submit.Click();
         }
@@ -41,9 +46,9 @@ namespace ConsoleApplication2
         {
             driver.Url = "https://portal.globallogic.com";
             IWebElement login = driver.FindElement(By.XPath("//*[@id='app']/div/div/div/div[2]/div/div/form/div[2]/div[1]/input"));
-            login.SendKeys("login");
+            login.SendKeys("olga.bykova");
             IWebElement pass = driver.FindElement(By.XPath("//*[@id='app']/div/div/div/div[2]/div/div/form/div[2]/div[2]/input"));
-            pass.SendKeys("password");
+            pass.SendKeys("Notulp1994");
             IWebElement submit = driver.FindElement(By.XPath("//*[@id='app']/div/div/div/div[2]/div/div/form/button"));
             submit.Click();
             Thread.Sleep(5000);
@@ -60,17 +65,14 @@ namespace ConsoleApplication2
         public void MainNavLinks()
 
         {
-
-            driver.Url = "https://portal.globallogic.com";
+            driver.Url = "https://portal.globallogic.com/";
             IWebElement login = driver.FindElement(By.XPath("//*[@id='app']/div/div/div/div[2]/div/div/form/div[2]/div[1]/input"));
-            login.SendKeys("login");
+            login.SendKeys("olga.bykova");
             IWebElement pass = driver.FindElement(By.XPath("//*[@id='app']/div/div/div/div[2]/div/div/form/div[2]/div[2]/input"));
-            pass.SendKeys("password");
+            pass.SendKeys("Notulp1994");
             IWebElement submit = driver.FindElement(By.XPath("//*[@id='app']/div/div/div/div[2]/div/div/form/button"));
             submit.Click();
             Thread.Sleep(5000);
-            
-
 
             IWebElement popup = driver.FindElement(By.Id("gl-tutorial-modal"));
             popup.FindElement(By.CssSelector("button[ng-click='close()']"));
@@ -82,7 +84,6 @@ namespace ConsoleApplication2
             IWebElement helpdesk = driver.FindElement(By.XPath("//*[@id='app']/div[2]/div[2]/div/div/div[1]/div/div/div[2]/div/a/div/img"));
             helpdesk.Click();
             
-
             IWebElement EPR = driver.FindElement(By.XPath("//*[@id='app']/div[2]/div[2]/div/div/div[1]/div/div/div[3]/div/a/div/img"));
             EPR.Click();
 
@@ -98,7 +99,7 @@ namespace ConsoleApplication2
         public void OfficeTime()
 
         {
-            driver.Url = "https://login:password@portal-ua.globallogic.com/officetime";
+            driver.Url = "https://olga.bykova:Notulp1994@portal-ua.globallogic.com/officetime/";
 
             IWebElement clickweek = driver.FindElement(By.XPath("//*[@id='toolbar']/a[1]"));
             clickweek.Click();
@@ -117,17 +118,17 @@ namespace ConsoleApplication2
 
             IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
             js.ExecuteScript("window.scrollTo(0, document.body.scrollHeight);");
-            Thread.Sleep(2000);
+            WebDriverWait wait2 = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
 
             IWebElement clickgraphview = driver.FindElement(By.XPath("//*[@id='stats']/a[4]"));
             clickgraphview.Click();
-            Thread.Sleep(2000);
+            WebDriverWait wait3 = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
         }
 
         [TearDown]
-        public void EndTestOfficeTime()
+        public void EndTest()
         {
-            driver.Close();
+                driver.Close();
         }
     }
 }
